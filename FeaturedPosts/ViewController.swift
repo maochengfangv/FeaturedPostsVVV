bea496e979b86cc43753433c7bec4de0c7e1e5af
maochengfangv <maochengfangv@gmail.com>
2026-06-24 15:15:44 +0800

    Initial Commit

---
+++ FeaturedPosts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeaturedPosts
+//
+//  Created by maochengfang on 2026/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
